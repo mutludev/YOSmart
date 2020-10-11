0c9f0ed96c61fc8d5fb3c3863e1a3afc2bbf6a10
mutludev <43097444+mutludev@users.noreply.github.com>
2020-10-11 23:26:17 +0300

    Added button to modal for Saving

--- src/components/UniversityModal.js
+++ src/components/UniversityModal.js
@@ -1,13 +1,16 @@
 import React, {useState} from 'react'
 import Modal from 'react-bootstrap/Modal'
 import UniversityForm from './UniversityForm'
+import { Button } from 'react-bootstrap'
 
 export default function UniversityModal ({isNew=true,show,handleClose}) {
 
-
+  function saveChanges(){
+    handleClose()
+  }
 
   return (
-    <Modal show={show} onHide={handleClose}>
+    <Modal show={show} onHide={handleClose} size="lg">
       <Modal.Header closeButton>
         <Modal.Title>
           {isNew ? "Add New University" : "Edit University"}
@@ -16,6 +19,11 @@ export default function UniversityModal ({isNew=true,show,handleClose}) {
       <Modal.Body>
         <UniversityForm/>
       </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={saveChanges}>
+          Save
+        </Button>
+      </Modal.Footer>
     </Modal>
   )
 }
