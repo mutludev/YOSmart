d55a5d74131826b1e2aa3aa75dc5688f15770838
mutludev <43097444+mutludev@users.noreply.github.com>
2020-12-13 13:24:47 +0300

    Translate parsing algorithm

--- src/components/universityParsingAlgorithm.js
+++ src/components/universityParsingAlgorithm.js
@@ -26,47 +26,47 @@ export default function universityParsingAlgorithm(data){
     //Apply Date
     let remainingApplyStartDate = Math.ceil((applyStartDate-currentDate)/86400000)
     if(remainingApplyStartDate<=maxDaysForNotifications && remainingApplyStartDate>=0){
-      news.push({title:`${universityName} sınav başvurularına ${remainingApplyStartDate} gün kaldı.`,status:"danger",days:remainingApplyStartDate})
+      news.push({title:`${remainingApplyStartDate} days left to start ${universityName} applications`,status:"danger",days:remainingApplyStartDate})
     }else if (remainingApplyStartDate>=-1){
-      news.push({title:`${universityName} sınav başvuruları bugün.`,status:"danger",days:remainingApplyStartDate})}
+      news.push({title:`${universityName} applications start today`,status:"danger",days:remainingApplyStartDate})}
 
 
 
       let remainingApplyEndDate = Math.ceil((applyEndDate-currentDate)/86400000)
       if(remainingApplyEndDate<=maxDaysForNotifications && remainingApplyEndDate>=0){
-        news.push({title:`${universityName} sınav başvuruların bitmesine ${remainingApplyEndDate} gün kaldı.`,status:"danger",days:remainingApplyEndDate})
+        news.push({title:`${remainingApplyStartDate} days left to the ${universityName} application deadline`,status:"danger",days:remainingApplyEndDate})
       }else if (remainingApplyEndDate>=-1){
-        news.push({title:`${universityName} sınav başvuruları bugün bitiyor.`,status:"danger",days:remainingApplyEndDate})}
+        news.push({title:`${universityName} application deadline is today`,status:"danger",days:remainingApplyEndDate})}
 
 
   //Exam Release Date
   let remainingExamResultsDate = Math.ceil((resultsDate-currentDate)/86400000)
   if(remainingExamResultsDate<=maxDaysForNotifications && remainingExamResultsDate>=0){
-    news.push({title:`${universityName} sınav sonuçlarının açıklanmasına ${remainingExamResultsDate} gün kaldı.`,status:"success",days:remainingExamResultsDate})
+    news.push({title:`${remainingExamResultsDate} days left to the announcement of ${universityName} exam results`,status:"success",days:remainingExamResultsDate})
   }else if (remainingExamResultsDate>=-1){
-    news.push({title:`${universityName} sınav sonuçları bugün açıklanıyor.`,status:"success",days:remainingExamResultsDate})}
+    news.push({title:`${universityName} exam results are announced today`,status:"success",days:remainingExamResultsDate})}
 
   //Select Date
   let remainingSelectStartDate = Math.ceil((selectStartDate-currentDate)/86400000)
   if(remainingSelectStartDate<=maxDaysForNotifications && remainingSelectStartDate>=0){
-    news.push({title:`${universityName} tercihlerinin açılmasına ${remainingSelectStartDate} gün kaldı.`,status:"warning",days:remainingSelectStartDate})
+    news.push({title:`${remainingSelectStartDate} days left to start of ${universityName} preferences`,status:"warning",days:remainingSelectStartDate})
   }else if (remainingSelectStartDate>=-1){
-    news.push({title:`${universityName} tercihleri bugün açılıyor.`,status:"warning",days:remainingSelectStartDate})}
+    news.push({title:`${universityName} preferences start today`,status:"warning",days:remainingSelectStartDate})}
 
 
     let remainingSelectEndDate = Math.ceil((selectEndDate-currentDate)/86400000)
   if(remainingSelectEndDate<=maxDaysForNotifications && remainingSelectEndDate>=0){
-    news.push({title:`${universityName} tercihlerinin kapanmasına ${remainingSelectEndDate} gün kaldı.`,status:"warning",days:remainingSelectEndDate})
+    news.push({title:`${remainingSelectEndDate} days left to the ${universityName} preferences deadline`,status:"warning",days:remainingSelectEndDate})
   }else if (remainingSelectEndDate>=-1){
-    news.push({title:`${universityName} tercihleri bugün kapanıyor.`,status:"warning",days:remainingSelectEndDate})}
+    news.push({title:`${universityName} preferences deadline is today`,status:"warning",days:remainingSelectEndDate})}
 
 
   //Select Release Date
     let remainingSelectionResultsDate = Math.ceil((selectionResultsDate-currentDate)/86400000)
   if(remainingSelectionResultsDate<=maxDaysForNotifications && remainingSelectionResultsDate>=0){
-    news.push({title:`${universityName} tercihlerinin açıklanmasına ${remainingSelectionResultsDate} gün kaldı.`,status:"success",days:remainingSelectionResultsDate})
+    news.push({title:`${remainingSelectionResultsDate} days left until the announcement of ${universityName} preferences`,status:"success",days:remainingSelectionResultsDate})
   }else if (remainingSelectionResultsDate>=-1){
-    news.push({title:`${universityName} tercihleri bugün açıklanıyor.`,status:"success",days:remainingSelectionResultsDate})}
+    news.push({title:`${universityName} preferences announced today`,status:"success",days:remainingSelectionResultsDate})}
 
   });
 
